9ff01f03963c5ed2edae1ee81c05122e5a1c1516
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-05-28 21:24:44 +0200

    add bg

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-var mymap = L.map('themap').setView([51.505, -0.09], 13);
+
+
+var baseMaps = {
+	"Ljus": L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVycmthcmxzb24iLCJhIjoiY2p1MW9td3ZpMDNrazQ0cGVmMDltc3EwaSJ9.0h6iBb8t7laIu-xP7YE4CQ', {
+		tileSize: 512,
+		zoomOffset: -1,
+	}),
+	"Normal": L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVycmthcmxzb24iLCJhIjoiY2p1MW9td3ZpMDNrazQ0cGVmMDltc3EwaSJ9.0h6iBb8t7laIu-xP7YE4CQ', {
+		tileSize: 512,
+		zoomOffset: -1,
+	}),
+	"Satellit": L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+		attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+	}),
+	"Mörk": L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVycmthcmxzb24iLCJhIjoiY2p1MW9td3ZpMDNrazQ0cGVmMDltc3EwaSJ9.0h6iBb8t7laIu-xP7YE4CQ'),
+}
+
+var map = L.map('map', {
+	//center: [59.3274541, 18.0543566],
+	//zoom: 13,
+	layers: [baseMaps['Normal']],
+	zoomControl: false,
+}).setView([18.505, 50.09], 13);
