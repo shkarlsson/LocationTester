50a48e4f12f72fa448d20c49e40f762e96a81f6d
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-05-28 21:30:10 +0200

    stamenToner

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ var baseMaps = {
 		attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 	}),
 	"Mörk": L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVycmthcmxzb24iLCJhIjoiY2p1MW9td3ZpMDNrazQ0cGVmMDltc3EwaSJ9.0h6iBb8t7laIu-xP7YE4CQ'),
+	"StamenToner": L.tileLayer('http://a.tile.stamen.com/toner/${z}/${x}/${y}')
 }
 
 var map = L.map('themap', {
 	//center: [59.3274541, 18.0543566],
 	//zoom: 13,
-	layers: [baseMaps['Ljus']],
+	layers: [baseMaps['StamenToner']],
 	zoomControl: false,
 }).setView([18.505, 50.09], 13);
