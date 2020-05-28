746f0b4c3f16ec1cc1e5a02c28fb95f1c6a0504e
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-05-28 21:40:24 +0200

    add colors

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+var colors = {
+	'red99': '#e6194B',
+	'green99': '#3cb44b',
+	'yellow100': '#ffe119',
+	'blue100': '#4363d8',
+	'orange9999': '#f58231',
+	'purple95': '#911eb4',
+	'cyan99': '#42d4f4',
+	'magenta99': '#f032e6',
+	'lime95': '#bfef45',
+	'pink9999': '#fabebe',
+	'teal99': '#469990',
+	'lavender9999': '#e6beff',
+	'brown99': '#9A6324',
+	'beige99': '#fffac8',
+	'maroon9999': '#800000',
+	'mint99': '#aaffc3',
+	'olive95': '#808000',
+	'apricot95': '#ffd8b1',
+	'navy9999': '#000075',
+	'grey100': '#a9a9a9',
+	'white100': '#ffffff',
+	'black100': '#000000',
+}
 
 
 var baseMaps = {
@@ -21,7 +45,7 @@ var map = L.map('themap', {
 	//zoom: 13,
 	layers: [baseMaps['Satellit']],
 	zoomControl: false,
-}).setView([18.505, 50.09], 13);
+}).setView([58.505, 18.09], 13);
 
 L.control.layers(null, baseMaps, {
 	position: 'topleft'
